3d16daa60bd39587bd2ecda11674b4ffa52cf84a
Anastasia Serebrennikova <Serebrennikovaav01@mail.ru>
2023-01-15 22:43:17 +0300

    add decoration

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import './Card.css';
 import { Image } from 'antd';
+import { format } from 'date-fns'
+import { Typography } from 'antd';
+const { Title, Paragraph, Text } = Typography;
+//import PropTypes from 'prop-types';
 
 const filmPoster = 'https://image.tmdb.org/t/p/w500';
 
@@ -8,21 +12,33 @@ export default class Card extends Component  {
   
 
   render(){
-    const { id, release_date, title, genre_ids ,overview,backdrop_path } = this.props;
+    const { id, release_date , title, genre_ids ,overview,backdrop_path } = this.props;
+
+    const releaseDate = format(new Date(release_date), 'MMMM dd, yyyy')
     
-    return (
-      <li key={id} className='card'>
-        <Image className="card__photo"
-          src={`${filmPoster}${backdrop_path}`}  alt="movie-zphoto"
-       />
-       <div className="card__description">
-        <h2>{title}</h2>
-        <span>{release_date} </span>
-        <p> {genre_ids}</p>
-        <p>{overview}</p>
-       </div>
-    </li>
-    );
-}
+      return (
+        <li key={id} className='card'>
+          <Image className="card__photo"
+          width={1000}
+          height={280}
+            src={`${filmPoster}${backdrop_path}`}  alt="movie-zphoto"
+        />
+        <Typography className="card__description">
+          <Title className="card__title"
+           level={3}>
+            {title}
+            </Title>
+          <span>{releaseDate} </span><br/>
+          <Text keyboard> {genre_ids}</Text>
+          <Paragraph className="card__plot">{overview}</Paragraph>
+        </Typography>
+
+      </li>
+      );
+  }
 }
 
+Card.defaultProps = {
+  title : 'No Title',
+  release_date: '01-01-01'
+}
